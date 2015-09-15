c07fa3a732682da0d905833faac08299942063b8
dansmith <dsmith@ltu.edu>
2015-09-14 22:24:17 -0400

    Added general, ide and opencv

---
+++ general.sh
@@ -0,0 +1,6 @@
+git config --global user.email "dsmith@ltu.edu"
+git config --global user.name "dansmith"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
---
+++ ide.sh
@@ -0,0 +1,11 @@
+sudo apt-get --force-yes -y install vim
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get --force-yes -y install sublime-text-installer
+sudo apt-get --force-yes -y install cmake build-essential clang
+git clone --recursive https://github.com/quarnster/SublimeClang ~/.config/sublime-text-3/Packages/SublimeClang
+cp /usr/lib/x86_64-linux-gnu/libclang-3.4.so.1 ~/.config/sublime-text-3/Packages/SublimeClang/internals/libclang.so
+mkdir ~/.config/sublime-text-3/Packages/SublimeClang/src/build
+cd ~/.config/sublime-text-3/Packages/SublimeClang/src/build
+cmake ..
+sudo make -j8
