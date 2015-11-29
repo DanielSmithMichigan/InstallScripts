7a2f67451e335870d3d84150eb679e2cf275f71e
dansmith <dsmith@ltu.edu>
2015-11-28 20:42:33 -0500

    Added libxtst-dev and curl

--- general.sh
+++ general.sh
@@ -5,7 +5,7 @@ git config --global user.name "dansmith"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
-sudo apt-get install --force-yes -y google-chrome-stable vlc
+sudo apt-get install --force-yes -y google-chrome-stable vlc curl
 sudo cp -rf tohome/* ~
 cat bashrc >> ~/.bashrc
 sudo source ~/.bashrc
--- opencv.sh
+++ opencv.sh
@@ -3,7 +3,7 @@ sudo apt-get --force-yes -y update
 sudo apt-get --force-yes -y upgrade
 sudo apt-get --force-yes -y install git build-essential cmake git pkg-config
 sudo apt-get --force-yes -y install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
-sudo apt-get --force-yes -y install libgtk2.0-dev
+sudo apt-get --force-yes -y install libgtk2.0-dev libxtst-dev
 sudo apt-get --force-yes -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get --force-yes -y install libatlas-base-dev gfortran
 mkdir ~/install
